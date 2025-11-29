cfc48c4dff25e6a48fdb3be3da6e52e8fa0058a6
iliya Bykov <osipv6@icloud.com>
2025-11-29 18:38:15 +0300

    Изменён протокол, добален код для CRUD и доработан модуль базы данных для MVP.

--- Habit Forge/Persistence/PersistenceManager.swift
+++ Habit Forge/Persistence/PersistenceManager.swift
@@ -11,7 +11,7 @@ import CoreData
 // Контракт
 protocol PersistenceManagerProtocol {
     func createHabit(name: String, icon: String) -> Habit?
-    func fetchHabits() -> [Habit]
+    func fetchHabits(with id: UUID) -> Habit?
     func updateHabit(_ habit: Habit)
     func deleteHabit(_ habit: Habit)
     
@@ -23,6 +23,7 @@ protocol PersistenceManagerProtocol {
 
 // Реализация для CoreData
 class PersistenceManager: PersistenceManagerProtocol {
+    
     
     static let shared = PersistenceManager()
     
@@ -44,13 +45,32 @@ class PersistenceManager: PersistenceManagerProtocol {
     // MARK: - Habit CRUD
     
     func createHabit(name: String, icon: String) -> Habit? {
-        <#code#>
+        let habit = Habit(context: context)
+        
+        habit.name = name
+        habit.icon = icon
+        habit.id = UUID()
+        habit.createdAt = Date()
+        
+        do {
+            try context.save()
+            return habit
+        } catch {
+            print("Ошибка создания привычки \(error)")
+            return nil
+        }
     }
     
-    func fetchHabits() -> [Habit] {
+    func fetchHabits(with id: UUID) -> Habit? {
         let request = NSFetchRequest<Habit>(entityName: "Habit")
-        request.predicate = NSPredicate(format: "id == %@", id as! CVarArg)
-        return try? container.viewContext.fetch(request).first
+        request.predicate = NSPredicate(format: "id == %&", id as CVarArg)
+        
+        do {
+            return try context.fetch(request).first   // возврат массива с привычками
+        } catch {
+            print("Ошибка загрузки привычки: \(error)")
+            return nil   // если ошибка - nil
+        }
     }
     
     func updateHabit(_ habit: Habit) {
@@ -58,15 +78,30 @@ class PersistenceManager: PersistenceManagerProtocol {
     }
     
     func deleteHabit(_ habit: Habit) {
-        <#code#>
+        context.delete(habit)
+        save()
     }
     
     func addCompletion(for habit: Habit, date: Date) -> Completion? {
-        <#code#>
+        let completion = Completion(context: context)
+        
+        completion.id = UUID()
+        completion.date = date
+        completion.habit = habit
+        
+        
+        do {
+            try context.save()
+            return completion
+        } catch {
+            print("Ошибка \(error)")
+            return nil
+        }
     }
     
     func deleteCompletion(_ completion: Completion) {
-        <#code#>
+        context.delete(completion)
+        save()
     }
     
     func save() {
